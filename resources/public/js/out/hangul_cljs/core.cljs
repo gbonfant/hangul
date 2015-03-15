a33ee0284b838a5229c74778e4b53c89f770e9a9
Gus Bonfant <gbonfant@me.com>
2015-03-15 02:36:55 +0000

    Implement correct/incorrect validation

--- resources/public/js/out/hangul_cljs/core.cljs
+++ resources/public/js/out/hangul_cljs/core.cljs
@@ -4,37 +4,67 @@
               [hangul-cljs.dictionary :as dictionary]))
 
 ;; -------------------------
-;; Views
+;; State
 
 (def guess (atom ""))
-(def generated-character
-  (let [hangul (dictionary/random-card)] hangul))
+(def show-notice (atom false))
+(def notice-message (atom ""))
+(def generated-card (atom (dictionary/random-card)))
+
+;; -------------------------
+;; Functions
 
 (defn hangul-character []
-  (let [character generated-character] (:hangul character)))
+  (let [character @generated-card] (:hangul character)))
+
+(defn reset-card-state []
+  (swap! generated-card dictionary/random-card)
+  (swap! guess (fn [] "")))
+
+(defn toggle-notice [message]
+  (swap! show-notice (fn [] true))
+  (swap! notice-message (fn [] message)))
+
+(defn show-success-notice []
+  (reset-card-state)
+  (toggle-notice "Correct!"))
+
+(defn show-failure-notice []
+  (toggle-notice "Incorrect!"))
+
+(defn verify []
+  (if (= (:roman @generated-card) @guess)
+    (show-success-notice)
+    (show-failure-notice)))
+
+;; -------------------------
+;; Components
 
 (defn hangul-input []
   [:input {:type "text"
            :value @guess
            :on-change #(reset! guess (-> % .-target .-value))}])
 
-(defn verify []
-  (.log js/console (= (:roman generated-character) @guess)))
-
-
 (defn submit-btn []
   [:input {:type "button"
            :value "Check"
            :on-click verify}])
 
+(defn success-notice []
+  (if @show-notice
+    [:div @notice-message]))
 
 (defn hangul-card []
   [:div.hangul-card
    [:div.hangul-character (hangul-character)]
    [:div.hangul-input (hangul-input) (submit-btn)]])
 
+(defn app []
+  [:div
+   [success-notice]
+   [hangul-card]])
 
 ;; -------------------------
 ;; Initialize app
 (defn init! []
-  (reagent/render-component [hangul-card] (.getElementById js/document "app")))
+  (reagent/render-component [app] (.getElementById js/document "app")))
